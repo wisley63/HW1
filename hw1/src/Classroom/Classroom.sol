95f038499d1e42f0f9bcb13a87d1c31cbda4a9ac
wisley63 <113053224+wisley63@users.noreply.github.com>
2024-03-20 21:08:30 +0800

    Update Classroom.sol

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -1,24 +1,18 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/* Problem 1 Interface & Contract */
 contract StudentV1 {
-    uint256 public code;
+    uint256 public registrationCode;
 
     function register() external returns (uint256) {
-        code = 123;
-        return code;
+        registrationCode = 123;
+        return registrationCode;
     }
 }
-
-/* Problem 2 Interface & Contract */
 contract StudentV2 {
     function register() external view returns (uint256) {
         return 123;
     }
 }
-
-/* Problem 3 Interface & Contract */
 contract StudentV3 {
     function register() external view returns (uint256) {
         return 123;
