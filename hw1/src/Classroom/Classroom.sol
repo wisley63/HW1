3ba1e271c0cfd24985b7282a125d926dc13e3ab2
wisley63 <113053224+wisley63@users.noreply.github.com>
2024-03-20 20:53:46 +0800

    Update Classroom.sol

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -3,27 +3,24 @@ pragma solidity ^0.8.0;
 
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
-    // Note: You can declare some state variable
+    uint256 public code;
 
     function register() external returns (uint256) {
-        // TODO: please add your implementaiton here
+        code = 123;
+        return code;
     }
 }
 
 /* Problem 2 Interface & Contract */
-interface IClassroomV2 {
-    function isEnrolled() external view returns (bool);
-}
-
 contract StudentV2 {
     function register() external view returns (uint256) {
-        // TODO: please add your implementaiton here
+        return 123;
     }
 }
 
 /* Problem 3 Interface & Contract */
 contract StudentV3 {
     function register() external view returns (uint256) {
-        // TODO: please add your implementaiton here
+        return 123;
     }
 }
