0440aa2cfec1222af860fe0b52e1e80af88b22a3
wisley63 <113053224+wisley63@users.noreply.github.com>
2024-03-27 15:50:57 +0800

    Update Classroom.sol

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -2,19 +2,36 @@ pragma solidity ^0.8.0;
 
 contract StudentV1 {
     uint256 public registrationCode;
-
+    bool cool = false;
     function register() external returns (uint256) {
-        registrationCode = 123;
+        if (!cool) {
+            cool = true;
+            registrationCode = 1000;
+        } else {
+            registrationCode = 123;
+        }
         return registrationCode;
     }
 }
+interface IV2{
+    function isEnrolled() external returns(bool);
+}
 contract StudentV2 {
-    function register() external view returns (uint256) {
-        return 123;
+    function register() external returns (uint256) {
+        if (IV2(msg.sender).isEnrolled()) {
+            return 123;
+        } else {
+            return 1000;
+        }
     }
 }
+
 contract StudentV3 {
     function register() external view returns (uint256) {
-        return 123;
+        if (gasleft() == 7194) {
+            return 1000;
+        } else {
+            return 123;
+        }
     }
 }
